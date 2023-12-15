ab3d2efae89678b60c19390f5ab217a47fb76613
Frank <fjbautistas@unal.edu.co>
2023-12-14 22:23:17 -0300

    Cambia el metodo de lectura, ya que numpy ocupaba mucha memoria

--- rdata.py
+++ rdata.py
@@ -4,28 +4,40 @@ from matplotlib import rcParams, colors, ticker, cm
 
 #leer archivo:
 class read_file: 
-    
+
     def __init__(self, path, file, columns=None):
         if columns is not None:
             self.cols = columns
         else:
             #self.cols     = [0, 1, 2, 3, 5, 6, 7]        #ID, hosthalo, substruc, masa y posiciones xyz
             self.cols      = [1, 2, 3]                    #solo N. de sub y masa para el plot 2D.
-        self.path = path; self.file = file; self.read()
-        
+        self.path = path; self.file = file; 
+        self.read()
+
     def read(self):
+        data = []
         if self.file == "*.hdf5":
             print("No se ha hecho esta parte")
         else:
-            self.data = np.genfromtxt(str(self.path+self.file), usecols = self.cols)#, names=True)   
-            #self.data = np.array([list(row) for row in data])#para poder hacer slicing
-            
+            with open(self.path + self.file, 'r') as file:
+                for line in file:
+                    split_line = line.split()  # Asumiendo que los datos están separados por espacios
+                    try:
+                        # Seleccionar solo las columnas deseadas y convertirlas a float
+                        selected_data = [float(split_line[i]) for i in self.cols]
+                        data.append(selected_data)
+                    except (ValueError, IndexError):
+                        # Manejar posibles errores de conversión o índices incorrectos
+                        continue
+        self.data = np.array(data)
+        #return np.array(self.data)
+                
     def halo_info(self, index, info=False): 
         self.hinfo = self.data[index]
         return(self.hinfo)
 
 #cinematica entre DOS halos
-class kinetic:                                   
+'''class kinetic:                                   
     
     def __init__(self, halo1, halo2):                       #halo1 y halo22 son objetos de read_file (halo_info)  
         self.halo1 = halo1; self.halo2 = halo2     
@@ -33,13 +45,13 @@ class kinetic:
     def distance(self): 
         posA = self.halo1[4:]; posB = self.halo2[4:]
         return(np.linalg.norm(posA-posB))
-    
+'''    
     #def velocity(self, , halo2): 
         #return(np.linalg.norm(halo1-halo2))
     
 
 #=======================================================================================================
-#if __name__ == '__main__':
+if __name__ == '__main__':
     path = 'DATA/'; file ='sussing_125.z0.000.AHF_halos'
     sussing_125 = read_file(path, file)
 
@@ -58,8 +70,8 @@ class kinetic:
     y = (yedges[1:]+yedges[:-1])/2
 
     #Escritura de archivo, primero H y luego los edges: 
-    np.savetxt('out_data/H_data.csv', H.flatten(), delimiter=',', fmt='%f', header='H')
-    np.savetxt('out_data/edges_data.csv', np.column_stack((xedges, yedges)), delimiter=',', fmt='%f', header='xedges,yedges')
+    #np.savetxt('out_data/H_data.csv', H.flatten(), delimiter=',', fmt='%f', header='H')
+    #np.savetxt('out_data/edges_data.csv', np.column_stack((xedges, yedges)), delimiter=',', fmt='%f', header='xedges,yedges')
 
     # Grafico con bines: 
     fig, ax = plt.subplots(1,1, figsize=(4, 4))
@@ -71,10 +83,12 @@ class kinetic:
                    extent=[xedges[0], xedges[-1], yedges[0],yedges[-1]], 
                    aspect='auto', interpolation='nearest') 
 
-    plt.yscale('log'); 
+    #plt.yscale('log'); 
     ax.set_xlabel(r'log$_{10}$(M$_{halo}$[$M_{0}h^{-1}$])')
     ax.set_ylabel("Number of subsetructures")
     plt.tight_layout()
-    plt.savefig("plot/hist2D.pdf")
+    plt.savefig("plot/hist2Db.pdf")
+
+    print('finalicé y todo bien')
 
 
